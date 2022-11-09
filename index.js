3769ad86044d8f131b00c14191cbfb0d0e53cc55
Uddin02 <mr.uddin360@gmail.com>
2022-11-09 15:33:29 +0600

    services api added

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const cors = require("cors");
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// console.log(process.env.DB_USER);
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.czp86h0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
+
+async function run(){
+    try {
+        const serviceCOllection = client.db("alphaTourDbUser").collection("services");
+
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCOllection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        
+        app.get('/allServices', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCOllection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+       
+        
+    } finally {
+    // await client.close();
+    }
+}
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
